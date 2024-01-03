7091c2f26a32706c5edad42efd4033dcb5236b81
tonytang <>
2024-01-03 15:21:33 +0800

    更新ts和tailwind.css配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   // or 'media' or 'class'
-  darkMode: false,
+  media: false,
   theme: {
     extend: {},
   },
